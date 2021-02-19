17c86f1ce7076eb2a5c9fab4de936baf1f116809
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-02-18 23:25:29 -0500

    first

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
 
+let button = document.querySelector("button");
+console.log(button);
+let brush = document.querySelector(".brush");
+console.log(brush.style);
+let input = document.querySelector('input');
+console.log(input);
+
+function handler(e) {
+  console.log(e);
+  e.preventDefault();
+  brush.style.backgroundColor = input.value;
+}
+
+button.addEventListener('click', handler);
