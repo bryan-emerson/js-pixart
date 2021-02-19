df538b4638f35877914bb9d66b043242731a9e0d
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-02-19 08:21:29 -0500

    four

--- pixart.js
+++ pixart.js
@@ -1,4 +1,4 @@
-
+//one
 let button = document.querySelector("button");
 console.log(button);
 let brush = document.querySelector(".brush");
@@ -14,10 +14,16 @@ function handler(e) {
 }
 
 button.addEventListener('click', handler);
+//skip two per Teo
 
-
+//three
 for (i = 0; i < 20; i ++) {
   let square = document.createElement('div');
   square.classList.add('square');
   body.appendChild(square);
+  square.addEventListener("click", function(){
+    square.style.background = 'green'
+  })
 }
+
+//four
